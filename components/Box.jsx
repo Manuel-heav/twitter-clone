819274e65b51a020f9a260fcce11096dee9386c6
manuel-heav <manuelheav32@gmail.com>
2022-03-19 09:35:51 -0400

    did infobar successfully too

---
+++ components/Box.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Box = () => {
+  return (
+    <div className='cursor-pointer'>
+        <div>
+            <p className='text-xs mt-5'>Trending in Ethiopia</p>
+            <p className='text-white'>Africans</p>
+            <p className='text-xs'>17.K Tweets</p>
+        </div>
+    </div>
+  )
+}
+
+export default Box
--- components/InfoBar.jsx
+++ components/InfoBar.jsx
@@ -1,10 +1,31 @@
 import React from 'react'
-import { SparklesIcon } from '@heroicons/react/outline'
+import { SearchIcon } from '@heroicons/react/outline'
+import Box from './Box'
 
 const InfoBar = () => {
   return (
-    <div className='hidden md:block'>
-      <h1 className="text-white">Hello</h1>
+    <div className='hidden md:block text-gray-400 overflow-y-scroll scrollbar-hide'>
+      <div className='flex mt-5 ml-10 rounded-full bg-[#202327] p-2'>
+        <SearchIcon className='h-9 text-white items-center p-2'/>
+        <input className="inline-flex flex ml-2 items-center bg-[#202327] flex-shrink outline-none"type="text" placeholder='Search Twitter' />
+      </div>
+
+      <div className='rounded-xl mt-5 ml-10 bg-[#202327] text-gray-400 p-7 h-1/2 overflow-y-scroll scrollbar-hide'>
+        <Box />
+        <Box />
+        <Box />
+        <Box />
+        <Box />
+        <Box />
+        <Box />
+        <Box />
+        <Box />
+        <Box />
+        <Box />
+        <Box />
+      </div>
+
+     
     </div>
   )
 }
