b5f2792453f6c0f874382460be505e565dbaeef5
manuel-heav <manuelheav32@gmail.com>
2022-03-20 08:53:19 -0400

    added file input

--- components/Center.jsx
+++ components/Center.jsx
@@ -16,11 +16,12 @@ const Center = () => {
       </div>
 
     <div className='flex items-center justify-between mt-7 border-b border-gray-700 pb-2'>
-      <div className='flex ml-1 md:ml-10'>
-        <PhotographIcon className="text-[#1d9bf0] h-9 w-9 hover:bg-gray-900 rounded-full p-2 cursor-pointer"/>
-        <ClipboardListIcon className="text-[#1d9bf0]  h-9 w-9 hover:bg-gray-900 rounded-full p-2 cursor-pointer"/>
-        <EmojiHappyIcon className="text-[#1d9bf0] h-9 w-9 hover:bg-gray-900 rounded-full p-2 cursor-pointer"/>
-        <LinkIcon className="text-[#1d9bf0] h-9 w-9 hover:bg-gray-900 rounded-full p-2 cursor-pointer"/>
+      <div className='flex items-center ml-1 md:ml-10'>
+        <input type="file" className="w-24"/>
+        <PhotographIcon className="hidden md:block text-[#1d9bf0] h-9 w-9 hover:bg-gray-900 rounded-full p-2 cursor-pointer"/>
+        <ClipboardListIcon className="hidden md:block text-[#1d9bf0]  h-9 w-9 hover:bg-gray-900 rounded-full p-2 cursor-pointer"/>
+        <EmojiHappyIcon className="hidden md:block text-[#1d9bf0] h-9 w-9 hover:bg-gray-900 rounded-full p-2 cursor-pointer"/>
+        <LinkIcon className="hidden md:block text-[#1d9bf0] h-9 w-9 hover:bg-gray-900 rounded-full p-2 cursor-pointer"/>
       </div>
       <button className="pl-7 pt-2 pb-2 pr-7 bg-[#1d9bf0] text-white rounded-full">Tweet</button>
     </div>
